05718aa8151a1a26b85e4585d0ed9d52de2a270f
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-10-21 16:40:13 +0300

    Add files via upload

---
+++ week02/week02-solutions/task0.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int num1, num2;
+	char choice;
+	
+	cout << "\nPlease, enter first number: ";
+	cin >> num1;
+	
+	cout << "\nPlease, enter second number: ";
+	cin >> num2;
+	
+	cout << "\nPlease, choose [+, -, *, /]: ";
+	cin >> choice;
+	
+	switch(choice){
+		
+		case '+': cout << num1 << " + " << num2 << " = " << num1 + num2;
+			break;
+			
+		case '-': cout << num1 << " - " << num2 << " = " << num1 - num2;
+			break;
+			
+		case '*': cout << num1 << " * " << num2 << " = " << num1 * num2;
+			break;
+			
+		case '/': if(num2 != 0){
+				  cout << num1 << " / " << num2 << " = " << num1 / num2;
+			break;
+		}else{
+				  cout << "You can't divide by 0!" << endl;
+			break;
+		}
+		
+		default: cout << "There is no such action!";
+	}
+	
+	
+	return 0;
+}
