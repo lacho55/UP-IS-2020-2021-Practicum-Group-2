0d1c6adca4172e19804883cfe6199c810304e299
lacho55 <lacho_fitness@abv.bg>
2020-12-28 00:22:02 +0200

    Adding task0.

---
+++ week09/week09-solutions/task0.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int* halfSize(const int *arr, int size)
+{
+ 	int newSize = size / 2;
+ 	int *newArr = new int[newSize];
+ 	
+ 	for(int i = 0; i < newSize; i++){
+ 		newArr[i] = arr[i];
+	 }
+ 	
+ 	return newArr;
+} 
+
+
+
+int main(){
+	int arr[4] = {1,2,3,4};
+	
+	int* newArr = halfSize(arr,4);
+	
+	
+	return 0;
+}
